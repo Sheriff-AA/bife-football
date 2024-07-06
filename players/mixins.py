6a9aea2d19d87d4e1dc759c8997506e4a084eb32
Sherif Atitebi <sherifatitebi@gmail.com>
2024-07-07 00:52:18 +0100

    mostly ui, removed email confirm for allauth

--- players/mixins.py
+++ players/mixins.py
@@ -6,7 +6,7 @@ class CoachRequiredMixin(AccessMixin):
     """Verify that the current user is authenticated and is a coach"""
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated or not request.user.is_coach:
-            return redirect("landing:page")
+            return redirect("players:player-list")
         return super().dispatch(request, *args, **kwargs)
     
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./templates/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        18: "4.5rem",
+        20: "5rem",
+        22: "5.5rem",
+        24: "6rem",
+        // Add more custom spacing values as needed
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
