8f73bc4c997dacad40c4daa308ab0b4ffccd7a2f
Sherif Atitebi <sherifatitebi@gmail.com>
2024-07-09 23:52:37 +0100

    handling validation in signup

--- organizations/forms.py
+++ organizations/forms.py
@@ -1,7 +1,7 @@
 from allauth.account.forms import SignupForm
 from django.db import IntegrityError
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.core.exceptions import ValidationError
 from django import forms
 from players.models import Team, Coach
 
@@ -11,18 +11,27 @@ class CustomSignupForm(SignupForm):
     team_name = forms.CharField(max_length=80, required=True, label='Name of Your Team')
     short_team_name = forms.CharField(max_length=3, required=True, label='Abbreviated 3 Letter Name For Your Team')
 
+    def clean_team_name(self):
+        team_name = self.cleaned_data.get('team_name')
+        if Team.objects.filter(team_name=team_name).exists():
+            raise ValidationError('A team with this name already exists.')
+        return team_name
+
+    def clean_short_team_name(self):
+        short_team_name = self.cleaned_data.get('short_team_name')
+        if Team.objects.filter(short_team_name=short_team_name).exists():
+            raise ValidationError('A team with this abbreviated name already exists.')
+        return short_team_name
+
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
         user.is_coach = True
         user.save()
-        try:
-            team = Team.objects.create(
-            team_name = self.cleaned_data['team_name'],
-            short_team_name = self.cleaned_data['short_team_name'],
-            organisation = user
+        team = Team.objects.create(
+        team_name = self.cleaned_data['team_name'],
+        short_team_name = self.cleaned_data['short_team_name'],
+        organisation = user
         )
-        except IntegrityError as e:
-            messages.error(request, "Action not permitted by user")
         Coach.objects.create(
             first_name = self.cleaned_data['first_name'],
             last_name = self.cleaned_data['last_name'],
