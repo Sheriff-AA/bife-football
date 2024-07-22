ae1b74b67876f05af7ae2999a0022bcc7d18ec8c
Sherif Atitebi <sherifatitebi@gmail.com>
2024-07-22 17:04:59 +0100

    allowed hosts again

--- bifefootball/settings.py
+++ bifefootball/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = config('DJANGO_DEBUG', cast=bool)
 
 ALLOWED_HOSTS = [
-    'https://soccerbase.onrender.com',
+    'soccerbase.onrender.com',
 ]
 
 if DEBUG:
